1c9f5d0a449a84d3c6eef0c98a44202330108b77
Michael Dalton <215679523+michael-dalton-home@users.noreply.github.com>
2025-07-21 16:24:55 +0100

    Create a new project and build a grid of 16x16 divs
    Initial html and css created, and work started on
    div grid in javascript.  Very much wip

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # etch-a-sketch
+
+Odin Project - 
+https://www.theodinproject.com/lessons/foundations-etch-a-sketch
+
+Timer: 
+20m basic html frame with css
---
+++ js/javascript.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const GRID_ROWS = 16;
+const GRID_COLS = 16;
+
+
+
+function build_new_grid(container) {
+
+    let y_divs, x_divs = [];
+    let new_row = undefined;
+    let new_item = undefined;
+
+    for (let cols=0; cols<GRID_COLS; cols++) {
+        // Div acting as row container
+        new_row = document.createElement('div');
+        new_row.classList.add('grid-rows');
+        container.appendChild(new_row);
+
+        // Populate new row with further child divs
+        for (let rows=0; rows<GRID_ROWS; rows++) {
+            new_item = document.createElement('div');
+            new_item.classList.add('grid-items');
+            new_row.appendChild(new_item);
+        }
+    }
+}
